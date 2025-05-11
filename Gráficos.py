67fc22dd843296a9ea6ea13403166457dc0d3730
MaxArayaE <max.araya.e@gmail.com>
2025-05-11 18:37:35 -0400

    Se agregan comentarios

--- Gráficos.py
+++ Gráficos.py
@@ -6,11 +6,16 @@ import os
 def main():
     #Conección a la Base de Datos, En caso de ser usado en tú computador, cambia las credenciales de abajo
     conn = psycopg2.connect(host="172.27.162.97", dbname="restaurantes", user="maxaraya", password="maximox12", port="5432")
+    
     platillosMasSolicitados(2025, conn)
     diasDeMayorDemanda(2025,conn)
+    
+    #Se cierra la conección a la base de datos
     conn.close()
 
+#Esta función recibe el año deseado y la conección a la base de datos, retorna un gráfico de barras con las estadísticas de cada platillo pedido en el año
 def platillosMasSolicitados(año, conn):
+    #Se crea el cursor para manipular las consultas
     cursor = conn.cursor()
 
     consulta = '''
@@ -21,18 +26,20 @@ def platillosMasSolicitados(año, conn):
     GROUP BY P."Nombre"
     ORDER BY total_pedidos DESC;
     '''
-
+    #Se le dice a la bd que ejecute el script
     cursor.execute(consulta, (año,))
     resultados = cursor.fetchall()
 
+    #Se cierra el cursor
     cursor.close()
+
+    #Se asegura de que existan datos para el año solicitado
+    if not resultados:
+        print(f"No se encontraron pedidos para el año {año}.")
+        return
 
     # Convertir resultados a DataFrame
     df = pandas.DataFrame(resultados, columns=["Platillo", "Cantidad"])
-
-    if df.empty:
-        print(f"No se encontraron pedidos para el año {año}.")
-        return
 
     # Graficar
     plt.figure(figsize=(10, 6))
@@ -48,7 +55,9 @@ def platillosMasSolicitados(año, conn):
     plt.savefig(guardado)
     plt.close()
 
+#Esta función recibe el año deseado y la conección a la base de datos, retorna un gráfico de torta con el porcentaje de demanda de los días en el año
 def diasDeMayorDemanda(año, conn):
+    #Se crea el cursor para manipular las consultas
     cursor = conn.cursor()
 
     consulta = '''
@@ -56,12 +65,14 @@ def diasDeMayorDemanda(año, conn):
     FROM "Hechos_Ordenes"
     WHERE EXTRACT(YEAR FROM "Fecha") = %s;
     '''
-
+    #Se le dice a la bd que ejecute el script
     cursor.execute(consulta, (año,))
     resultados = cursor.fetchall()
 
+    #Se cierra el cursor
     cursor.close()
 
+    #Se asegura que existan datos para el año solicitado
     if not resultados:
         print(f"No se encontraron pedidos para el año {año}.")
         return
@@ -69,6 +80,7 @@ def diasDeMayorDemanda(año, conn):
     # Convertir resultados a DataFrame
     df = pandas.DataFrame(resultados, columns=["dia_semana"])
 
+    #Se le asigna un ID al día
     mapeo_dias = {
         0: "Domingo",
         1: "Lunes",
