6bc46ec4edad670e9fc97b53d3f414aeb9c1e7fe
MaxArayaE <max.araya.e@gmail.com>
2025-05-18 16:52:28 -0400

    Arreglado error de rellenar.py

--- rellenar.py
+++ rellenar.py
@@ -6,35 +6,34 @@ from dotenv import load_dotenv
 # Cargar variables del archivo .env
 load_dotenv()
 
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
+datos_bd={ 
+        "host": os.getenv("DB_HOST"),
+        "port": os.getenv("DB_PORT"),
+        "dbname": os.getenv("DB_NAME"),
+        "user": os.getenv("DB_USER"),
+        "password": os.getenv("DB_PASSWORD"),
+        }
 
 CSV_FOLDER = "Datos de la BD"
 SCHEMA_FILE = "Restaurante.sql"
 
-def conectar_base():
-    return psycopg2.connect(
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASS,
-        host=DB_HOST,
-        port=DB_PORT
-    )
+conn = psycopg2.connect(**datos_bd)
 
 def ejecutar_esquema(conn):
-    cursor = conn.cursor()
-    with open(SCHEMA_FILE, 'r', encoding='utf-8') as f:
-        esquema_sql = f.read()
-        try:
-            cursor.execute(esquema_sql)
-            print("Esquema ejecutado correctamente.")
-        except Exception as e:
-            print(f"Error ejecutando esquema: {e}")
-            conn.rollback()
-    cursor.close()
+ cursor = conn.cursor()
+ with open(SCHEMA_FILE, 'r', encoding='utf-8') as f:
+    esquema_sql = f.read()
+    try:
+        for statement in esquema_sql.split(';'):
+            statement = statement.strip()
+            if statement:
+                cursor.execute(statement + ';')
+            conn.commit()
+    except Exception as e:
+        print(f"Error ejecutando esquema: {e}")
+        conn.rollback()
+ cursor.close()
+
 
 def insertar_csv(conn, archivo_csv):
     nombre_tabla = os.path.splitext(os.path.basename(archivo_csv))[0]
@@ -44,7 +43,7 @@ def insertar_csv(conn, archivo_csv):
         print(f"Archivo '{archivo_csv}' está vacío. Saltando...")
         return
 
-    columnas = ','.join(df.columns)
+    columnas = ','.join([f'"{col}"' for col in df.columns])
     valores = [tuple(x) for x in df.to_numpy()]
     placeholders = ','.join(['%s'] * len(df.columns))
 
@@ -72,8 +71,6 @@ orden_tablas = [
 ]
 
 # Ejecutar flujo completo
-conn = conectar_base()
-
 if os.path.exists(SCHEMA_FILE):
     ejecutar_esquema(conn)
 
